90746e541d10e92563f73bdffa6e9e68d8b6df91
italorhan <italorhan@gmail.com>
2022-11-29 18:28:13 -0300

    Delete variables directory

--- variables/_background.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Variáveis utilizadas para instalação do 4ZAP
-
-#Cor
-readonly BG_BROWN="\e[0;43m"
--- variables/_general.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Variáveis utilizadas para instalação do 4ZAP
-
-#Reset de cor
-readonly NC="\033[0m"
