df8a5d393f584e0580f72770269de116f3eb3608
italorhan <italorhan@gmail.com>
2022-11-30 15:21:07 -0300

    Add files via upload

---
+++ utils/_banner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "\n"
+
+  printf "${GREEN}";
+  printf "██   ██        ███████     ███     ███████ \n";
+  printf "██   ██            ███    ██ ██    ██   ██ \n";
+  printf "███████  ████    ██      ██   ██   ███████ \n";
+  printf "     ██        ██       ██ ███ ██  ██      \n";
+  printf "     ██        ███████ ██       ██ ██      \n";
+  printf "${NC}";
+
+  printf "\n"
+}
